b3cf99894d82a3a502e848dbfe66b3a808e4cfda
Vaidehi Bhardwaj <61655574+Vaidehi0421@users.noreply.github.com>
2021-04-02 18:23:01 +0530

    Added
    Phew

--- routes/company.js
+++ routes/company.js
@@ -10,3 +10,42 @@ router.get('/', isLoggedIn, isAdmin, catchAsync(async(req,res,next) => {
     const companies=await Company.find({});
     res.render('companies/allcompany',{companies});
 }))
+
+router.get('/:id',isLoggedIn,catchAsync(async(req,res,next)=>{
+    const { id }=req.params;
+    const company = await Company.findById(id);
+    res.render('companies/show',{company});
+}))
+
+//to verify a company
+router.get('/:id/verify', isLoggedIn, isAdmin, catchAsync(async(req,res,next) => {
+    const { id } = req.params;
+    const company = await Company.findById(id);
+    company.verified = true;
+    await Company.findByIdAndUpdate(id, company);
+    res.redirect(`/companies/${company._id}`);
+}))
+
+//to show the edit form of company 
+router.get('/:id/edit',isLoggedIn,catchAsync(async (req,res,next) => {
+    const { id } = req.params;
+    const company = await Company.findById(id);
+    if(req.user.username===company.username)
+        res.render('companies/edit', { company });
+    else
+        throw new ExpressError("You are not allowed to access this page",401);
+}))
+
+//to save the edited details of companies 
+router.put('/:id',isLoggedIn, catchAsync(async (req,res,next)=>{
+    const { id } = req.params;
+    let company=await Company.findById(id);
+    if(req.user.username===company.username){
+        company = await Company.findByIdAndUpdate(id, {...req.body.companies});
+        res.redirect(`/companies/${company._id}`);  
+    }
+    else
+        throw new ExpressError("You are not allowed to access this page",401);
+}))
+
+module.exports=router;
